3ece896afee1bf1a58012d7e73991193a420c3d1
KuznetsovaOD <marselin1806@gmail.com>
2021-10-25 23:56:38 +0300

    Добавьте файлы проекта.

---
+++ Lab_2_Patterns/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Lab_2_Patterns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bauhaus 93", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(399, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.BackColor = System.Drawing.Color.Blue;
+            this.comboBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(284, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 26);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите колличество сторон фигуры, которую хотите нарисовать от 0 до 5";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(402, 327);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 463);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Нарисуй фигуру";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab_2_Patterns/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2_Patterns
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)   //controller
+        {
+            ShapeFactory factory = new ShapeFactory();
+            Shape ShapePolygon = factory.get(Convert.ToInt32(comboBox1.Text));
+            try
+            {
+                pictureBox1.Image = ShapePolygon.Draw();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Ошибка"); }
+            label2.Text = ShapePolygon.Descriptor();
+        }
+
+        public int getHeight()
+        {
+            return pictureBox1.Height;
+        }
+        public int getWidth()
+        {
+            return pictureBox1.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "0";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public class ShapeFactory
+    {
+        public Shape get(int numberOfSides)
+        {
+            if (numberOfSides == 0)
+            {
+                return new Circle();
+            }
+            else if (numberOfSides == 1)
+            {
+                return new Line();
+            }
+            else if (numberOfSides == 2)
+            {
+                return new Angle();
+            }
+            else if (numberOfSides == 3)
+            {
+                return new Triangle();
+            }
+            else if (numberOfSides == 4)
+            {
+                return new Square();
+            }
+            else
+            {
+                return new Pentagon();
+            }
+        }
+    }
+
+
+    abstract public class Shape
+    {
+        public int x1;
+        public int y1;
+        public SolidBrush pen;
+        public Pen myPen;
+        public Shape()
+        {
+            x1 = 100;
+            y1 = 100;
+            pen = new SolidBrush(Color.White);
+            myPen = new Pen(Color.Purple);
+        }
+        public abstract string Descriptor();
+        abstract public Image Draw();
+    }
+
+
+    public class Circle : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawEllipse(myPen, x1, y1, 100, 100);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Круг"; }
+    }
+
+
+    public class Line : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawLine(myPen, x1, y1, 200, 200);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Линия"; }
+    }
+
+
+    public class Angle : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawLine(myPen, 100, 100, 200, 200);
+            graph.DrawLine(myPen, 200, 200, 100, 200);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Угол"; }
+    }
+
+
+    public class Triangle : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+
+            PointF point1 = new PointF(100, 100);
+            PointF point2 = new PointF(200, 200);
+            PointF point3 = new PointF(100, 200);
+            PointF[] curvePoints =
+                     {
+                                point1,
+                                point2,
+                                point3,
+                             };
+            graph.DrawPolygon(myPen, curvePoints);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Треугольник"; }
+    }
+
+
+    public class Square : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            graph.DrawRectangle(myPen, 100, 100, 100, 100);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Квадрат"; }
+    }
+
+
+    public class Pentagon : Shape
+    {
+        public override Image Draw()
+        {
+            Form1 form1 = new Form1();
+            Bitmap pictureBox = new Bitmap(form1.getWidth(), form1.getHeight());
+            Graphics graph = Graphics.FromImage(pictureBox);
+            graph.FillRectangle(pen, form1.ClientRectangle);
+            PointF point1 = new PointF(150, 100);
+            PointF point2 = new PointF(200, 140);
+            PointF point3 = new PointF(180, 200);
+            PointF point4 = new PointF(120, 200);
+            PointF point5 = new PointF(100, 140);
+            PointF[] curvePoints =
+                     {
+                                point1,
+                                point2,
+                                point3,
+                                point4,
+                                point5,
+                             };
+            graph.DrawPolygon(myPen, curvePoints);
+            return pictureBox;
+        }
+        public override string Descriptor() { return "Пентагон"; }
+    }
+}
+
